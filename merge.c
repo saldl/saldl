9f8b9bb000909dbd81994b8ac6bc6a83af0511bc
Mohammad Alsaleh <CE.Mohammad.AlSaleh@gmail.com>
2015-09-02 23:11:41 +0300

    Add copyright and license info to source files
    Signed-off-by: Mohammad Alsaleh <CE.Mohammad.AlSaleh@gmail.com>

--- merge.c
+++ merge.c
@@ -1,3 +1,22 @@
+/*
+    This file is a part of saldl.
+
+    Copyright (C) 2014-2015 Mohammad AlSaleh <CE.Mohammad.AlSaleh at gmail.com>
+    https://github.com/saldl/saldl
+
+    saldl is free software: you can redistribute it and/or modify
+    it under the terms of the Affero GNU General Public License as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Affero GNU General Public License for more details.
+
+    You should have received a copy of the Affero GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "events.h"
 
 static void merge_finished_cb(evutil_socket_t fd, short what, void *arg) {
--- resume.h
+++ resume.h
@@ -1,3 +1,22 @@
+/*
+    This file is a part of saldl.
+
+    Copyright (C) 2014-2015 Mohammad AlSaleh <CE.Mohammad.AlSaleh at gmail.com>
+    https://github.com/saldl/saldl
+
+    saldl is free software: you can redistribute it and/or modify
+    it under the terms of the Affero GNU General Public License as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Affero GNU General Public License for more details.
+
+    You should have received a copy of the Affero GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef _SALDL_RESUME_H
 #define _SALDL_RESUME_H
 #else
