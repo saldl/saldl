b79f2530b4286ca8d70c3ad7be793498ee99d805
Mohammad Alsaleh <CE.Mohammad.AlSaleh@gmail.com>
2015-10-05 11:48:01 +0300

    saldl: Free content_type
    Signed-off-by: Mohammad Alsaleh <CE.Mohammad.AlSaleh@gmail.com>

--- src/saldl.c
+++ src/saldl.c
@@ -30,6 +30,7 @@ static void saldl_free_all(info_s *info_ptr) {
   /* Make valgrind happy */
   saldl_free(info_ptr->chunks);
   saldl_free(info_ptr->threads);
+  saldl_free(info_ptr->content_type);
   saldl_free(params_ptr->filename);
   saldl_free(params_ptr->attachment_filename);
   saldl_free(params_ptr->inline_cookies);
