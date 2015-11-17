f41fa4e6817c4f6387d94128e2eb617cf1124d14
Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>
2015-11-18 00:20:59 +0200

    OSX: Address audit offenses
    Signed-off-by: Mohammad AlSaleh <CE.Mohammad.AlSaleh@gmail.com>

--- OSX/saldl.rb
+++ OSX/saldl.rb
@@ -2,7 +2,6 @@ class Saldl < Formula
   desc "CLI downloader optimized for speed and early preview."
   homepage "https://saldl.github.io"
   url "https://github.com/saldl/saldl/archive/v31.tar.gz"
-  version "31"
   sha256 "146a980ae2109a391c7a8ab6a1c525458db9bf18f3f0477731c9b876630078b4"
 
   head do
@@ -31,7 +30,7 @@ class Saldl < Formula
     # a2x/asciidoc needs this to build the man page successfully
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
-    args = [ "--prefix=#{prefix}" ]
+    args = ["--prefix=#{prefix}"]
 
     # head uses git describe to acquire a version
     args << "--saldl-version=v#{version}" unless build.head?
